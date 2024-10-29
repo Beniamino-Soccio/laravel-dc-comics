178d79956b503b02eb2abff528f89caf1a37ed7d
Beniamino-Soccio <beniamino43@gmail.com>
2024-10-29 17:47:22 +0100

    added Role model - contoller - migration - seeeder

---
+++ database/migrations/2024_10_29_162731_create_roles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("class");
+            $table->string("race");
+            $table->string("damage_type");
+            $table->string("gender");
+            $table->tinyInteger("armor_class")->unsigned();
+            $table->string("starter_weapon");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
+
